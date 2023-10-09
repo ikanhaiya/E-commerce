26f63e8a7a84dba46b67860d2c451c0fe739383b
Kanhaiya <ikanhaiya2001@gmail.com>
2023-10-10 01:36:06 +0530

    header component and it style added, logo added and linked to home page

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import { Route, Routes } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
-
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
 
     <div>
-
+      <Header /> 
+      {/* header will always be on top because react-dom will manipulate routes component children */}
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
