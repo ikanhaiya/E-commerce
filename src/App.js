3546e783a3f62c08490a6acf9f6332806489ab83
Kanhaiya <ikanhaiya2001@gmail.com>
2023-10-11 00:20:58 +0530

    Sign-in and Sign up page added and Sign-in Component's basic structure is created

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { Route, Routes } from 'react-router-dom';
 
 
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 
@@ -18,6 +19,7 @@ function App() {
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/shop' element={<ShopPage />} />
+        <Route path='/signin' element={<SignInAndSignUpPage />} />
       </Routes>
     </div>
   );
