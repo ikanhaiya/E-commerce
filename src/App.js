f402552569b6f144c59cdc1159c3284f12d02e63
Kanhaiya <ikanhaiya2001@gmail.com>
2023-10-18 22:12:41 +0530

    sign up component added to the sign-in and sign-up page

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
-import { auth,createUserProfileDocument } from './firebase/firebase.utils'; // using this we want to store state of user in our APP
+import { auth, createUserProfileDocument } from './firebase/firebase.utils'; // using this we want to store state of user in our APP
 
-class  App extends React.Component {
+class App extends React.Component {
 
   constructor() {
     super();
@@ -20,7 +20,7 @@ class  App extends React.Component {
       currentUser: null
     }
   }
-  
+
   unsubscribeFromAuth = null
 
 
@@ -28,39 +28,59 @@ class  App extends React.Component {
     // onAuthStateChanged is function provided by auth service of firbase.
     // it take function as parameter telling what the state of the Auth on our firebase project is
 
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {  // subscription open
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {  // subscription open
+
+      if(userAuth){
+         
+        const userRef = await createUserProfileDocument(userAuth);
+
+        // we are going to check if our database is updated at userRef
+        
+        userRef.onSnapshot(snapShot => {
+           // setState is asynchronous function
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data() // get back object representing the values
+            }
+
+          });
+        });
+        
+      }
+
+      this.setState({currentUser: userAuth}); // setting currentUser to null if userAuth is NULL
     });
   }
 
   componentWillUnmount() {  // called when component is about to be removed.
 
-  
+
     this.unsubscribeFromAuth();
     // this method is used to clean subscription from firebase authentication state 
     //if not done so can result in memory leaks , cleaning up subscription when it is no longer needed
     // helps the component to have updated info about current users authentication state
-    
+
   }
 
 
 
-  render(){
-  return (
+  render() {
+    return (
 
-    <div>
-       {/* by this we are letting header component know wheater user is signed in or not 
+      <div>
+        {/* by this we are letting header component know wheater user is signed in or not 
        info of currentUser is passed as props  */}
-      <Header currentUser={this.state.currentUser} /> 
-      {/* header will always be on top because react-dom will manipulate routes component children */}
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/shop' element={<ShopPage />} />
-        <Route path='/signin' element={<SignInAndSignUpPage />} />
-      </Routes>
-    </div>
-  );
-}
+        <Header currentUser={this.state.currentUser} />
+        {/* header will always be on top because react-dom will manipulate routes component children */}
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/shop' element={<ShopPage />} />
+          <Route path='/signin' element={<SignInAndSignUpPage />} />
+        </Routes>
+      </div>
+    );
+  }
 }
 
 export default App;
